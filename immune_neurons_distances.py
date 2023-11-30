e4e04c8d646d94ac7a77cbcf968e5d42e3b41560
Nikhil I <56198218+unikill066@users.noreply.github.com>
2023-11-29 18:39:33 -0600

    update immune_neurons_distances.py
    now the charts generated are based on pixel-coordinates

--- immune_neurons_distances.py
+++ immune_neurons_distances.py
@@ -3,7 +3,7 @@ from logger import my_logger
 import constants as constants
 from datetime import datetime
 from scipy.spatial import distance
-import pandas as pd, matplotlib as mpl, os, click, random, string
+import pandas as pd, matplotlib as mpl, os, click, random, string, json
 from generate_plots import plot_immune_neur_distances, generate_scatter_plot_for_final_neuronal_barcodes
 
 
@@ -30,6 +30,11 @@ def calculate_distances(neurons_df, final_df, dist_path, stat_path):
 
 
 def random_generator(run_path):
+    """
+    This function generated a random 10 character-length string, which is then used to create a directory.
+    :param run_path: Directory path where the directory needs to be created to check and generate a unique random string
+    :return: returns a unique 10 char-length string
+    """
     try:
         my_logger.info("<---- Random generator function is initiated ---->")
         # Checking and generating random variable of length 10
@@ -57,19 +62,24 @@ def random_generator(run_path):
               help='Directory path to neuronal barcodes - Neuronal Barcodes')
 @click.option("--file_path", prompt="Enter file-path where Processed_files directory needs to be generated",
               type=str, help='Directory path where processed files need to be saved')
-@click.option("--barcodes_of_interest", prompt="Enter the barcodes of interest (Ex. CROT,PHTF2,USP13,FSCN1,SPTBN1,UCK2,ZFAND2B)",
+@click.option("--barcodes_of_interest", prompt="Enter the barcodes of interest (Ex. CROT,PHTF2,USP13,FSCN1,etc.)",
               type=str, help='Directory path where processed files need to be saved', default=None)
-@click.option("--scaling_factor", prompt="Scaling factor for the distance matrix",
-              type=int, help='Scaling factor for the distance matrix', default=100)
-def generate_immune_neuronal_distance_matrix(final_matrix_fp: str, neuronal_barcodes_identity_fp: str,
-                                             file_path: str, barcodes_of_interest: str=None, scaling_factor: int=100):
+# @click.option("--scaling_factor", prompt="Scaling factor for the distance matrix",
+#               type=int, help='Scaling factor for the distance matrix', default=100)
+def generate_immune_neuronal_distance_matrix(final_matrix_fp: str,
+                                             neuronal_barcodes_identity_fp: str,
+                                             file_path: str,
+                                             barcodes_of_interest: str):
     """
+        This method generated visium plots
+        :param final_matrix_fp:
+        :param neuronal_barcodes_identity_fp:
+        :param file_path:
+        :param barcodes_of_interest:Genes separated by a comma; Ex.: SNAP25,OSM
 
-    :param final_matrix_fp:
-    :param neuronal_barcodes_identity_fp:
-    :param file_path:
-    :param barcodes_of_interest:
-    :return:
+               Note: There should be no spaces in the barcodes_of_interest
+
+        :return: A response template with Status: success or failed
     """
     try:
         start_time = datetime.now()
@@ -88,16 +98,15 @@ def generate_immune_neuronal_distance_matrix(final_matrix_fp: str, neuronal_barc
 
         my_logger.info("<---- Generating immune neural distance matrix's and plots started ---->")
 
-        # Checking the corresponding file(s)
-        if not len(os.listdir(final_matrix_fp)) == len(os.listdir(neuronal_barcodes_identity_fp)):
-            raise Exception(constants.FILES_NOT_MATCHING)
         # Exception handling
         if not os.path.exists(final_matrix_fp):
             raise Exception("Final_matrix path: {} does not exist".format(final_matrix_fp))
         if not os.path.exists(neuronal_barcodes_identity_fp):
             raise Exception("Neuronal_barcodes path: {} does not exist".format(neuronal_barcodes_identity_fp))
-        if scaling_factor == 0:
-            raise Exception("Scaling factor cannot be set to 0, please try again.")
+
+        # Checking the corresponding file(s)
+        if not len(os.listdir(final_matrix_fp)) == len(os.listdir(neuronal_barcodes_identity_fp)):
+            raise Exception(constants.FILES_NOT_MATCHING)
 
         # Checking and creating the directories
         if not os.path.exists(runs_fp):
@@ -126,16 +135,32 @@ def generate_immune_neuronal_distance_matrix(final_matrix_fp: str, neuronal_barc
         my_logger.info("Neuronal barcodes identity filepath: {}".format(neuronal_barcodes_identity_fp))
         my_logger.info("Processed files filepath: {}, this is where the output files and plots are saved".format(file_path))
         my_logger.info("Barcodes of interest: {}".format(barcodes_of_interest))
-        my_logger.info("Scaling factor: {}".format(final_matrix_fp))
+
+        metadata_df = pd.read_csv('metadata.csv', names=[constants.final_mat, constants.neuronal_barcodes,
+                                                         constants.SEX, constants.scaling_fac], skiprows=1)
 
         if barcodes_of_interest is not None:
-            for final_matrix_file, neuronal_identity_file in zip(sorted(os.listdir(final_matrix_fp)),
-                                                                 sorted(os.listdir(neuronal_barcodes_identity_fp))):
+            for final_matrix_file, neuronal_identity_file, scaling_factor_dir in zip(metadata_df[constants.final_mat],
+                                                                                 metadata_df[constants.neuronal_barcodes],
+                                                                                 metadata_df[constants.scaling_fac]):
+                # Scaling Factor
+                with open(os.path.join(scaling_factor_dir, [i for i in os.listdir(scaling_factor_dir) if i.endswith(".json")][0]), "r") as f:
+                    json_data = json.load(f)
+                scaling_factor = 65 / json_data['spot_diameter_fullres']
+                my_logger.info("Scaling factor: {}".format(final_matrix_fp))
+
+                # Generate pixel co-ordinates
+                final_matrix_df = pd.read_csv(os.path.join(final_matrix_fp, final_matrix_file), names=col_names, header=None)
+                pixel_df = pd.read_csv(os.path.join(scaling_factor_dir, [i for i in os.listdir(scaling_factor_dir) if i.endswith(".csv")][0]),
+                                       names=["Barcodes", "Z", "X", "Y", "x_pixel", "y_pixel"])
+                temp_pixel_mer_df = final_matrix_df.merge(pixel_df, on="Barcodes", how="left")   # final_matrix_df.merge(pixel_df, on="Barcodes", how="left").head()
+                final_matrix_df = temp_pixel_mer_df[["Barcodes", "x_pixel", "y_pixel", "Gene ID", "Gene Name", "Gene Expression"]]
+                final_matrix_df = final_matrix_df.rename(columns={"x_pixel": "X", "y_pixel": "Y"})
+
                 mat, neur = str(final_matrix_file.split('.')[0]), str(neuronal_identity_file.split('.')[0])
-                neuronal_barcode_df = pd.read_csv(neuronal_barcodes_identity_fp + os.sep + neuronal_identity_file)
+                neuronal_barcode_df = pd.read_csv(os.path.join(neuronal_barcodes_identity_fp, neuronal_identity_file))
                 neurons_list = neuronal_barcode_df[constants.BARCODES].to_list()
-                final_matrix_df = pd.read_csv(final_matrix_fp + os.sep + final_matrix_file, names=col_names,
-                                              header=None)
+                # Filtering final matrix based on the neuron indent file barcodes
                 neurons_df = final_matrix_df[final_matrix_df[constants.BARCODES].isin(neurons_list)]
 
                 for barcode in sorted(list(set(barcodes_of_interest.split(',')))):
@@ -149,6 +174,7 @@ def generate_immune_neuronal_distance_matrix(final_matrix_fp: str, neuronal_barc
                             my_logger.warning("<---- This gene is not found in the current data: {} ---->".format(barcode))
                             continue
                     except Exception as error_msg:
+                        my_logger.info(error_msg)
                         raise Exception(
                             "Barcode {} does not exist in the data, please try again with a different barcode.".format(
                                 barcode))
@@ -157,6 +183,14 @@ def generate_immune_neuronal_distance_matrix(final_matrix_fp: str, neuronal_barc
                     generate_scatter_plot_for_final_neuronal_barcodes(final_matrix_fp, neuronal_barcodes_identity_fp,
                                                                       final_matrix_file, neuronal_identity_file, barcode,
                                                                       plots_filepath)
+
+                    '''
+                    
+                    
+                    Additional code can be added here, where final_matrix and neuronal_indent files are needed
+                    
+                    
+                    '''
 
                     updated_final_matrix_df = updated_final_matrix_df.loc[:, [constants.BARCODES,
                                                                               constants.X, constants.Y]]
@@ -169,52 +203,24 @@ def generate_immune_neuronal_distance_matrix(final_matrix_fp: str, neuronal_barc
                         (distance.cdist(neurons_coord_df, updated_final_matrix_df, 'euclidean') * scaling_factor),
                         index=neurons_coord_df.index, columns=updated_final_matrix_df.index)
 
-                    temp_dist_file_path = matrix_fp + os.sep + mat + '_' + neur + '_' + str(barcode) + '_distance_matrix' + constants.CSV_FILE
-                    temp_stats_file_path = matrix_fp + os.sep + mat + '_' + neur + '_' + str(barcode) + '_stats' + constants.CSV_FILE
+                    temp_dist_file_path = os.path.join(matrix_fp, mat + '_' + neur + '_' + str(barcode) + '_distance_matrix' + constants.CSV_FILE)
+                    temp_stats_file_path = os.path.join(matrix_fp, mat + '_' + neur + '_' + str(barcode) + '_stats' + constants.CSV_FILE)
 
-                    if not os.path.exists(plots_data_dir + os.sep + barcode):
-                        os.mkdir(plots_data_dir + os.sep + barcode)
-                    temp_dist_plots_path = plots_data_dir + os.sep + barcode + os.sep + mat + '_' + neur + '_distance_matrix' + constants.CSV_FILE
-                    temp_stats_plots_path = plots_data_dir + os.sep + barcode + os.sep + mat + '_' + neur + '_stats' + constants.CSV_FILE
-
-                    if scaling_factor == 100:
-                        temp_df.to_csv(temp_dist_file_path)
-                        temp_df.to_csv(temp_dist_plots_path)
-                        temp_df[constants.MEAN], temp_df[constants.MEDIAN] = temp_df.mean(axis=1), temp_df.median(axis=1)
-                        temp_df[constants.MIN], temp_df[constants.MAX] = temp_df.min(axis=1), temp_df.max(axis=1)
-                        temp_df = temp_df.loc[:, [constants.MEAN, constants.MEDIAN, constants.MIN, constants.MAX]]
-                        temp_df[constants.IDENT] = [
-                            neuronal_barcode_df[neuronal_barcode_df[constants.BARCODES] == i][constants.IDENT].values[0]
-                            for i in temp_df.index]
-                        temp_df[constants.CELL_TYPE] = constants.NEURONS
-                        temp_df.to_csv(temp_stats_file_path)
-                        temp_df.to_csv(temp_stats_plots_path)
-                    else:
-                        temp_df_cust_df = pd.DataFrame(
-                            (distance.cdist(neurons_coord_df, updated_final_matrix_df, 'euclidean') * 100),
-                            index=neurons_coord_df.index, columns=updated_final_matrix_df.index)
-                        temp_df.to_csv(temp_dist_file_path)
-                        temp_df_cust_df.to_csv(temp_dist_plots_path)
-                        temp_df[constants.MEAN], temp_df[constants.MEDIAN] = temp_df.mean(axis=1), temp_df.median(axis=1)
-                        temp_df[constants.MIN], temp_df[constants.MAX] = temp_df.min(axis=1), temp_df.max(axis=1)
-                        temp_df = temp_df.loc[:, [constants.MEAN, constants.MEDIAN, constants.MIN, constants.MAX]]
-                        temp_df.to_csv(temp_stats_file_path)
-                        temp_df_cust_df[constants.MEAN] = temp_df_cust_df.mean(axis=1)
-                        temp_df_cust_df[constants.MEDIAN] = temp_df_cust_df.median(axis=1)
-                        temp_df_cust_df[constants.MIN] = temp_df_cust_df.min(axis=1)
-                        temp_df_cust_df[constants.MAX] = temp_df_cust_df.max(axis=1)
-                        temp_df_cust_df = temp_df_cust_df.loc[:, [constants.MEAN, constants.MEDIAN,
-                                                                  constants.MIN, constants.MAX]]
-                        temp_df[constants.IDENT] = [
-                            neuronal_barcode_df[neuronal_barcode_df[constants.BARCODES] == i][constants.IDENT].values[0]
-                            for i in temp_df.index]
-                        temp_df_cust_df[constants.IDENT] = [
-                            neuronal_barcode_df[neuronal_barcode_df[constants.BARCODES] == i][constants.IDENT].values[0]
-                            for i in temp_df_cust_df.index]
-                        temp_df[constants.CELL_TYPE] = constants.NEURONS
-                        temp_df_cust_df[constants.CELL_TYPE] = constants.NEURONS
-                        temp_df.to_csv(temp_stats_file_path)
-                        temp_df_cust_df.to_csv(temp_stats_plots_path)
+                    if not os.path.exists(os.path.join(plots_data_dir, barcode)):
+                        os.mkdir(os.path.join(plots_data_dir, barcode))
+                    temp_dist_plots_path = os.path.join(plots_data_dir, barcode, mat + '_' + neur + '_distance_matrix' + constants.CSV_FILE)
+                    temp_stats_plots_path = os.path.join(plots_data_dir, barcode, mat + '_' + neur + '_stats' + constants.CSV_FILE)
+                    temp_df.to_csv(temp_dist_file_path)
+                    temp_df.to_csv(temp_dist_plots_path)
+                    temp_df[constants.MEAN], temp_df[constants.MEDIAN] = temp_df.mean(axis=1), temp_df.median(axis=1)
+                    temp_df[constants.MIN], temp_df[constants.MAX] = temp_df.min(axis=1), temp_df.max(axis=1)
+                    temp_df = temp_df.loc[:, [constants.MEAN, constants.MEDIAN, constants.MIN, constants.MAX]]
+                    temp_df[constants.IDENT] = [
+                        neuronal_barcode_df[neuronal_barcode_df[constants.BARCODES] == i][constants.IDENT].values[0]
+                        for i in temp_df.index]
+                    temp_df[constants.CELL_TYPE] = constants.NEURONS
+                    temp_df.to_csv(temp_stats_file_path)
+                    temp_df.to_csv(temp_stats_plots_path)
 
         else:
             raise Exception("No barcodes are provided to compare with the given data.")
@@ -245,15 +251,7 @@ if __name__ == '__main__':
     generate_immune_neuronal_distance_matrix()
 
 # Testing
-# C:\Users\NXI220005\Desktop\Visium_spatial_analysis_eucl_distances\final_matrix_space_ranger
-# C:\Users\NXI220005\Desktop\Visium_spatial_analysis_eucl_distances\neurons_ident
-# C:\Users\NXI220005\Desktop\Visium_spatial_analysis_eucl_distances
-# CD4,CROT,PHTF2,USP13,ADCK1,FSCN1,SPTBN1,POLG,TSR2,UCK2,ZFAND2B
-# GPR34,CD163,SEMA6B,OSM,HBEGF,PDPN,CXCL3,CXCL2,IL1A,CH25H,CHI3L
-# "CD68,CD163,OSM"
-# /Users/nikhil/Downloads/Visium_spatial_analysis-2/final_matrix_space_ranger
-# /Users/nikhil/Downloads/Visium_spatial_analysis-2/neurons_ident
-# /Users/nikhil/Downloads/Visium_spatial_analysis-2
-# generate_immune_neuronal_distance_matrix("/Users/nikhil/Downloads/Visium_spatial_analysis-2/final_matrix_space_ranger",
-#                                          "/Users/nikhil/Downloads/Visium_spatial_analysis-2/neurons_ident",
-#                                          "/Users/nikhil/Downloads/Visium_spatial_analysis-2", "OSM,CD4", 23)
+# generate_immune_neuronal_distance_matrix(final_matrix_fp=r"C:\Users\{username}\Downloads\euc_dist\f",
+#                                          neuronal_barcodes_identity_fp=r"C:\Users\{username}\Downloads\euc_dist\n",
+#                                          file_path=r"C:\Users\{username}\Downloads\euc_dist",
+#                                          barcodes_of_interest="SNAP25,CD4")  # barcodes should be separated by a ","
